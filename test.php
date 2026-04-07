1ccfd8fcce5c69f21e52599b4d77b991147cd5cd
Naveus <keremkaan099@gmail.com>
2026-04-07 13:09:21 +0300

    Add test.php with 15 echos

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+echo "Satır 1: PHP Echo Denemesi\n";
+echo "Satır 2: PHP Echo Denemesi\n";
+echo "Satır 3: PHP Echo Denemesi\n";
+echo "Satır 4: PHP Echo Denemesi\n";
+echo "Satır 5: PHP Echo Denemesi\n";
+echo "Satır 6: PHP Echo Denemesi\n";
+echo "Satır 7: PHP Echo Denemesi\n";
+echo "Satır 8: PHP Echo Denemesi\n";
+echo "Satır 9: PHP Echo Denemesi\n";
+echo "Satır 10: PHP Echo Denemesi\n";
+echo "Satır 11: PHP Echo Denemesi\n";
+echo "Satır 12: PHP Echo Denemesi\n";
+echo "Satır 13: PHP Echo Denemesi\n";
+echo "Satır 14: PHP Echo Denemesi\n";
+echo "Satır 15: PHP Echo Denemesi\n";
+?>
